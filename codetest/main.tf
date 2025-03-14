5a8598a2cb986078d4b473868887bbd8a918e394
bhas29 <bhavaniselvarajah299@gmail>
2025-03-14 15:49:46 +0530

    testing with chattestcode

--- codetest/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket" "mybucket" {
-bucket = "test-bucket"
-acl = "public-read"   # 👈 This is a security risk (lint issue)
-}
-
-resource "aws_instance" "myinstance" {ami="ami-12345678" instance_type="t2.micro"}   # 👈 Bad formatting
--- codetest2/azure_rbac/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output the created role assignments, exposing the resource details for further use.
-output "role_assignments" {
-  value = azurerm_role_assignment.role
-}
-
-# Output the created custom role definitions, allowing access to their properties.
-output "role_definitions" {
-  value = azurerm_role_definition.custom
-}
